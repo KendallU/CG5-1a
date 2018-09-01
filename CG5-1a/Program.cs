923d82e3423ca1de0a8b537e9e9e7345c0786b4a
KendallU <kendallunion@gmail.com>
2018-09-01 13:53:13 -0500

    Add project files.

---
+++ CG5-1a/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace CG_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool displayName = true;
+            while (displayName)
+            {
+                displayName = FullName();
+            }
+        }
+
+        private static bool FullName()
+        {
+
+            Console.WriteLine("What is your first name?");
+            Console.Write("Type name here:");
+            string firstName;
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            Console.Write("Type name here:");
+            string lastName;
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Choose how to display name:");
+            Console.WriteLine("1) First Last");
+            Console.WriteLine("2) Last First");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                FullName1(firstName, lastName);
+                Console.WriteLine("Hello, " + firstName + lastName);
+                Console.ReadLine();
+                return true;
+
+
+
+
+            }
+            else if (result == "2")
+            {
+                FullName2(lastName, firstName);
+                Console.WriteLine("Hello," + lastName + firstName);
+
+                Console.ReadLine();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+
+        private static string FullName2(string firstName, string lastName)
+
+        {
+            string lastFirst = lastName + firstName;
+            return lastFirst;
+
+
+
+
+
+        }
+        private static string FullName1(string firstName, string lastName)
+        {
+            string firstLast = firstName + lastName;
+            return firstLast;
+
+        }
+    }
+
+
+
+}
